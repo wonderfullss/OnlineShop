ada9cd1e8aaa3eee3ad544f448c26db08dbd0ed7
wonderfullss <dkrechin@list.ru>
2023-04-27 16:16:42 +0400

    пополнение баланса

--- src/main/java/com/example/onlineshop/Controller/AdminController.java
+++ src/main/java/com/example/onlineshop/Controller/AdminController.java
@@ -3,8 +3,8 @@ package com.example.onlineshop.Controller;
 import com.example.onlineshop.Service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +14,7 @@ public class AdminController {
     private final AdminService adminService;
 
     @PutMapping(value = "/admin/balance")
-    public ResponseEntity<?> upBalance(@PathVariable String username, @PathVariable Double balance) {
+    public ResponseEntity<?> upBalance(@RequestParam String username, @RequestParam Double balance) {
         return adminService.upBalance(username, balance);
     }
 }
