ed33778a2155c207acfc5924c89c6f76238cdc84
wonderfullss <dkrechin@list.ru>
2023-04-28 18:25:53 +0400

    Добавлена логика добавления товара для организации

--- src/main/java/com/example/onlineshop/Service/OrganizationService.java
+++ src/main/java/com/example/onlineshop/Service/OrganizationService.java
@@ -1,4 +1,36 @@
 package com.example.onlineshop.Service;
 
+import com.example.onlineshop.Entity.Organization;
+import com.example.onlineshop.Entity.Product;
+import com.example.onlineshop.Entity.User;
+import com.example.onlineshop.Repository.OrganizationRepository;
+import com.example.onlineshop.Repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class OrganizationService {
+
+    private final OrganizationRepository organizationRepository;
+
+    private final ProductRepository productRepository;
+
+    public ResponseEntity<?> addProduct(Product product, String orgName) {
+        Organization organization = organizationRepository.findByName(orgName);
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (organization == null)
+            return new ResponseEntity<>(Map.of("message", "Такой организации нет!"), HttpStatus.NOT_FOUND);
+        if (!organization.getUser().getId().equals(user.getId())) {
+            return new ResponseEntity<>(Map.of("message", "Нет доступа к организации"), HttpStatus.FORBIDDEN);
+        }
+        product.setOrganization(organization);
+        productRepository.save(product);
+        return new ResponseEntity<>(Map.of("message", "Товар успешно добавлен."), HttpStatus.OK);
+    }
 }
