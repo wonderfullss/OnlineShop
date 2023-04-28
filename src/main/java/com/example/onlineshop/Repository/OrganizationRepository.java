bfd1f1c140fbad8dce041670388e518feed9e072
wonderfullss <dkrechin@list.ru>
2023-04-28 18:26:05 +0400

    init

--- src/main/java/com/example/onlineshop/Repository/OrganizationRepository.java
+++ src/main/java/com/example/onlineshop/Repository/OrganizationRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrganizationRepository extends CrudRepository<Organization, Long> {
+
+    Organization findByName(String name);
 }
