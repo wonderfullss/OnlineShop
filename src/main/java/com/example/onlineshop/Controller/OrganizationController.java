7fd869ad3084eb9ace98f8363b2c868afdccdaef
wonderfullss <dkrechin@list.ru>
2023-04-28 18:26:24 +0400

    Контроллер добавления товара для организации

--- src/main/java/com/example/onlineshop/Controller/OrganizationController.java
+++ src/main/java/com/example/onlineshop/Controller/OrganizationController.java
@@ -1,4 +1,22 @@
 package com.example.onlineshop.Controller;
 
+import com.example.onlineshop.Entity.Product;
+import com.example.onlineshop.Service.OrganizationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class OrganizationController {
+
+    private final OrganizationService organizationService;
+
+    @PostMapping(value = "/organization/product/add")
+    public ResponseEntity<?> addProduct(@RequestBody Product product, @RequestParam String orgName) {
+        return organizationService.addProduct(product, orgName);
+    }
 }
