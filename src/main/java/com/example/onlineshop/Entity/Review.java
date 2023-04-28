4611c0e866155cf4954808662f1b442cd2346339
wonderfullss <dkrechin@list.ru>
2023-04-28 18:24:47 +0400

    сущность отзыва

---
+++ src/main/java/com/example/onlineshop/Entity/Review.java
@@ -0,0 +1,23 @@
+package com.example.onlineshop.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "review")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
