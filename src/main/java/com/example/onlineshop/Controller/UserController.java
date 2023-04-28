13ec75361527e5edde87d113652053cd927bd842
wonderfullss <dkrechin@list.ru>
2023-04-28 18:23:46 +0400

    контролер регистрации организации

--- src/main/java/com/example/onlineshop/Controller/UserController.java
+++ src/main/java/com/example/onlineshop/Controller/UserController.java
@@ -1,11 +1,14 @@
 package com.example.onlineshop.Controller;
 
+import com.example.onlineshop.Entity.Organization;
 import com.example.onlineshop.Entity.User;
 import com.example.onlineshop.Service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +21,10 @@ public class UserController {
     public ResponseEntity<?> getAllNotifications() {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return userService.allNotificationsForUser(user.getId());
+    }
+
+    @PostMapping(value = "/register/organization")
+    public ResponseEntity<?> registerOrganization(@RequestBody Organization organization) {
+        return userService.registerOrganization(organization);
     }
 }
