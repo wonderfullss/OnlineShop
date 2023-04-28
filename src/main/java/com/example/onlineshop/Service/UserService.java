090fae7400c0e1792bda2a2d4c6c58f36ab2fc4f
wonderfullss <dkrechin@list.ru>
2023-04-28 18:23:32 +0400

    Логика регистрации организации

--- src/main/java/com/example/onlineshop/Service/UserService.java
+++ src/main/java/com/example/onlineshop/Service/UserService.java
@@ -1,9 +1,13 @@
 package com.example.onlineshop.Service;
 
+import com.example.onlineshop.Entity.Organization;
+import com.example.onlineshop.Entity.User;
 import com.example.onlineshop.Repository.NotificationRepository;
+import com.example.onlineshop.Repository.OrganizationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +16,16 @@ public class UserService {
 
     private final NotificationRepository notificationRepository;
 
+    private final OrganizationRepository organizationRepository;
+
     public ResponseEntity<?> allNotificationsForUser(Long id) {
         return new ResponseEntity<>(notificationRepository.findAllByUserId(id), HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> registerOrganization(Organization organization) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        organization.setUser(user);
+        organizationRepository.save(organization);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
