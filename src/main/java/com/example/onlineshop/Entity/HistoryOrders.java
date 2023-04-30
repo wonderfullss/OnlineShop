c9bac66075bb99c499aec38e2b22a651b287d629
wonderfullss <dkrechin@list.ru>
2023-04-30 20:24:30 +0400

    Создал сущность истории заказов

---
+++ src/main/java/com/example/onlineshop/Entity/HistoryOrders.java
@@ -0,0 +1,34 @@
+package com.example.onlineshop.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "history_of_orders")
+public class HistoryOrders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long id;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private LocalDateTime date = LocalDateTime.now();
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
