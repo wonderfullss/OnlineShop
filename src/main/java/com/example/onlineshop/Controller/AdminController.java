a2d843b99bb81340f16a931aa12b28b929bcd315
wonderfullss <dkrechin@list.ru>
2023-05-04 17:36:32 +0400

    просмотр данных пользователя

--- src/main/java/com/example/onlineshop/Controller/AdminController.java
+++ src/main/java/com/example/onlineshop/Controller/AdminController.java
@@ -4,16 +4,23 @@ import com.example.onlineshop.Entity.Notification;
 import com.example.onlineshop.Service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class AdminController {
 
     private final AdminService adminService;
+
+    @GetMapping(value = "/admin/orderList/{id}")
+    public ResponseEntity<?> getUserOrderList(@PathVariable Long id) {
+        return adminService.getOrderList(id);
+    }
+
+    @GetMapping(value = "/admin/user/{id}")
+    public ResponseEntity<?> getUser(@PathVariable Long id) {
+        return adminService.getUser(id);
+    }
 
     @PutMapping(value = "/admin/balance")
     public ResponseEntity<?> upBalance(@RequestParam String username, @RequestParam Double balance) {
--- src/main/java/com/example/onlineshop/Service/AdminService.java
+++ src/main/java/com/example/onlineshop/Service/AdminService.java
@@ -1,7 +1,9 @@
 package com.example.onlineshop.Service;
 
+import com.example.onlineshop.Entity.HistoryOrders;
 import com.example.onlineshop.Entity.Notification;
 import com.example.onlineshop.Entity.User;
+import com.example.onlineshop.Repository.HistoryOrdersRepository;
 import com.example.onlineshop.Repository.NotificationRepository;
 import com.example.onlineshop.Repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -10,7 +12,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +23,22 @@ public class AdminService {
     private final UserRepository userRepository;
 
     private final NotificationRepository notificationRepository;
+
+    private final HistoryOrdersRepository historyOrdersRepository;
+
+    public ResponseEntity<?> getUser(Long id) {
+        User user = userRepository.findUserById(id);
+        if (user == null)
+            return new ResponseEntity<>(Map.of("message", "Пользователь не найден."), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> getOrderList(Long id) {
+        List<HistoryOrders> historyOrders = historyOrdersRepository.findAllByUserId(id);
+        if (userRepository.findById(id).isEmpty())
+            return new ResponseEntity<>(Map.of("message", "Пользователь не найден."), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(historyOrders, HttpStatus.OK);
+    }
 
     public ResponseEntity<?> upBalance(String username, Double balance) {
         User user = userRepository.findUserByUsername(username);
