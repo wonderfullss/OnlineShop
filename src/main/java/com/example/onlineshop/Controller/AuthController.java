0dcfd616e3908deb03c74cdf835398e68731c39c
wonderfullss <dkrechin@list.ru>
2023-05-05 19:13:17 +0400

    some fix

--- src/main/java/com/example/onlineshop/Controller/AuthController.java
+++ src/main/java/com/example/onlineshop/Controller/AuthController.java
@@ -21,7 +21,7 @@ public class AuthController {
         return authService.signUpUser(user);
     }
 
-    @PostMapping(value = "/shop/signin")
+    @PostMapping(value = "/shop/sign-in")
     public ResponseEntity<?> login(@RequestParam String email, @RequestParam String password) {
         return authService.singInUser(email, password);
     }
